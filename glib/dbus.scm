344db58f98221b72167eaab53f28996abb898eca
Neil Jerram <neil@ossau.homelinux.net>
2000-01-01 00:29:23 +0000

    dbus: add dbus-call-async

--- glib/dbus.scm
+++ glib/dbus.scm
@@ -24,8 +24,10 @@
   #:use-module (rnrs bytevectors)
   #:use-module (ice-9 format)
   #:use-module (ossau trc)
+  #:use-module (oop goops)
   #:export (dbus-interface
 	    dbus-call
+	    dbus-call-async
 	    dbus-connect
 	    dbus-interface-release))
 
@@ -85,33 +87,111 @@
 			    '*		; error
 			    )))
 
+(define g_dbus_proxy_call
+  (pointer->procedure void
+		      (dynamic-func "g_dbus_proxy_call" gio)
+		      (list '*		; proxy
+			    '*		; method_name
+			    '*		; parameters
+			    int		; flags
+			    int		; timeout_msec
+			    '*		; cancellable
+			    '*		; callback
+			    '*		; user_data
+			    )))
+
+(define g_dbus_proxy_call_finish
+  (pointer->procedure '*
+		      (dynamic-func "g_dbus_proxy_call_finish" gio)
+		      (list '*		; proxy
+			    '*		; res
+			    '*		; error
+			    )))
+
 (define (dbus-call interface method . parameters)
   (trc 'dbus-call interface method parameters)
-  (let ((parms (scheme->variant parameters))
-	(errloc (uint-list->bytevector '(0)
-				       (native-endianness)
-				       (sizeof '*))))
-    (trc 'dbus-call-parms (variant->string parms) parms)
-    (let* ((result (g_dbus_proxy_call_sync interface
-					   (string->pointer method)
-					   parms
-					   0
-					   10000
-					   %null-pointer
-					   (bytevector->pointer errloc)))
-	   (gerror (make-pointer (car (bytevector->uint-list errloc
-							     (native-endianness)
-							     (sizeof '*))))))
-      (trc 'dbus-call-result (variant->string result) result)
-      (trc 'dbus-call-error gerror)
-      (or (null-pointer? gerror)
-	  (let ((parsed-error (parse-c-struct gerror (list int32 int '*))))
-	    (trc 'dbus-error-code (cadr parsed-error))
-	    (trc 'dbus-error-msg (pointer->string (caddr parsed-error)))
-	    (throw 'dbus-error
-		   (cadr parsed-error)
-		   (pointer->string (caddr parsed-error)))))
-      (variant->scheme result))))
+  (let ((timeout 10000))
+    (if (and (pair? parameters)
+	     (eq? (car parameters) #:timeout))
+	(begin
+	  (set! timeout (cadr parameters))
+	  (set! parameters (cddr parameters))))
+    (let ((parms (scheme->variant parameters))
+	  (errloc (uint-list->bytevector '(0)
+					 (native-endianness)
+					 (sizeof '*))))
+      (trc 'dbus-call-parms (variant->string parms) parms)
+      (let* ((result (g_dbus_proxy_call_sync interface
+					     (string->pointer method)
+					     parms
+					     0
+					     timeout
+					     %null-pointer
+					     (bytevector->pointer errloc)))
+	     (gerror (make-pointer (car (bytevector->uint-list errloc
+							       (native-endianness)
+							       (sizeof '*))))))
+	(trc 'dbus-call-result (variant->string result) result)
+	(trc 'dbus-call-error gerror)
+	(or (null-pointer? gerror)
+	    (let ((parsed-error (parse-c-struct gerror (list int32 int '*))))
+	      (trc 'dbus-error-code (cadr parsed-error))
+	      (trc 'dbus-error-msg (pointer->string (caddr parsed-error)))
+	      (throw 'dbus-error
+		     (cadr parsed-error)
+		     (pointer->string (caddr parsed-error)))))
+	(variant->scheme result)))))
+
+(define interface-callbacks (make-object-property))
+
+(define (dbus-call-async completion-proc interface method . parameters)
+  (trc 'dbus-call-async interface method parameters)
+  (let* ((timeout 10000)
+	 (callback (lambda (source_object res user_data)
+		     (let* ((errloc (uint-list->bytevector '(0)
+							   (native-endianness)
+							   (sizeof '*)))
+			    (result (g_dbus_proxy_call_finish interface
+							      res
+							      (bytevector->pointer errloc)))
+			    (gerror (make-pointer (car (bytevector->uint-list errloc
+									      (native-endianness)
+									      (sizeof '*))))))
+		       (trc 'dbus-call-async-result (variant->string result) result)
+		       (trc 'dbus-call-async-error gerror)
+		       (completion-proc (if (null-pointer? gerror)
+					    (variant->scheme result)
+					    #f)
+					(if (null-pointer? gerror)
+					    #f
+					    (let ((parsed-error (parse-c-struct gerror (list int32 int '*))))
+					      (trc 'dbus-call-async-error-code (cadr parsed-error))
+					      (trc 'dbus-call-async-error-msg (pointer->string (caddr parsed-error)))
+					      (cons (cadr parsed-error)
+						    (pointer->string (caddr parsed-error)))))))))
+	 (ptr (procedure->pointer void
+				  callback
+				  (list '* '* '*))))
+    (set! (interface-callbacks interface)
+	  (acons callback
+		 ptr
+		 (or (interface-callbacks interface)
+		     '())))
+    (if (and (pair? parameters)
+	     (eq? (car parameters) #:timeout))
+	(begin
+	  (set! timeout (cadr parameters))
+	  (set! parameters (cddr parameters))))
+    (let ((parms (scheme->variant parameters)))
+      (trc 'dbus-call-async-parms (variant->string parms) parms)
+      (g_dbus_proxy_call interface
+			 (string->pointer method)
+			 parms
+			 0
+			 timeout
+			 %null-pointer
+			 ptr
+			 %null-pointer))))
 
 (define interface-signal-id (make-object-property))
 (define interface-signal-alist (make-object-property))
