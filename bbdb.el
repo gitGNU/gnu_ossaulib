1da6565fa8b7df164e5fb13807164f24d900f97e
Neil Jerram <neil@ossau.uklinux.net>
2009-05-31 19:32:47 +0100

    Export unhandled BBDB fields individually, instead of a combined vector

--- bbdb.el
+++ bbdb.el
@@ -15,14 +15,14 @@
     (while bbdb-entry
       (let ((first-names (aref bbdb-entry 0))
 	    (last-name (aref bbdb-entry 1))
+	    (x-field-2 (aref bbdb-entry 2)) ;AKAs
+	    (x-field-3 (aref bbdb-entry 3)) ;???
 	    (phone-numbers (aref bbdb-entry 4))
-	    (other-bbdb-info (vector (aref bbdb-entry 2) ;AKAs
-				     (aref bbdb-entry 3) ;???
-				     (aref bbdb-entry 5) ;addresses
-				     (aref bbdb-entry 6) ;emails
-				     (aref bbdb-entry 7) ;notes
-				     (aref bbdb-entry 8) ;???
-				     )))
+	    (x-field-5 (aref bbdb-entry 5)) ;addresses
+	    (x-field-6 (aref bbdb-entry 6)) ;emails
+	    (x-field-7 (aref bbdb-entry 7)) ;notes
+	    (x-field-8 (aref bbdb-entry 8)) ;???
+	    )
 	(let* ((contact-name (concat "_" first-names "_" last-name))
 	       (contact-file (concat outdir contact-name)))
 	  (save-excursion
@@ -37,10 +37,20 @@
 		(insert "PHONE " (aref phone 0) "\n"
 			" " (aref phone 1) "\n"))
 	      (setq phone-numbers (cdr phone-numbers)))
-	    (insert "X-OTHER-BBDB-INFO\n"
-		    " ")
-	    (prin1 other-bbdb-info (current-buffer))
-	    (insert "\n")
+	    (let ((x-fields (list (cons 2 x-field-2)
+				  (cons 3 x-field-3)
+				  (cons 5 x-field-5)
+				  (cons 6 x-field-6)
+				  (cons 7 x-field-7)
+				  (cons 8 x-field-8)))
+		  x-field)
+	      (while x-fields
+		(setq x-field (car x-fields)
+		      x-fields (cdr x-fields))
+		(insert "X-BBDB-FIELD-" (number-to-string (car x-field)) "\n"
+			" ")
+		(prin1 (cdr x-field) (current-buffer))
+		(insert "\n")))
 	    (write-region (point-min) (point-max) contact-file))))
       (setq bbdb-entry (condition-case nil
 			   (read (current-buffer))
