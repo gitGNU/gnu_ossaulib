827f3b59845c759f593f5fd88e3f593b652a4d61
Neil Jerram <neil@ossau.uklinux.net>
2009-12-24 00:19:49 +0000

    Move generic contact reading code to BBDB-independent module
    * ossau/contact.scm (ossau): New module, providing `fold-contacts',
      using code that was previously in bbdb-to.scm.
    
    * ossau/bbdb-to.scm (ossau): Use `fold-contacts'.

--- ossau/bbdb-to.scm
+++ ossau/bbdb-to.scm
@@ -1,9 +1,7 @@
 
 (define-module (ossau bbdb-to)
-  #:use-module (ice-9 ftw)
   #:use-module (ice-9 popen)
-  #:use-module (ice-9 rdelim)
-  #:use-module (ice-9 regex)
+  #:use-module (ossau contact)
   #:export (bbdb-to))
 
 (define-macro (assert condition)
@@ -20,47 +18,7 @@
 		       install-prefix))))
     (with-output-to-port pipe
       (lambda ()
-	(nftw dir maybe-import-contact)))
+	(fold-contacts dir #f (lambda (contact acc)
+				(write contact)
+				(newline)))))
     (close-pipe pipe)))
-
-(define (maybe-import-contact filename statinfo flag base level)
-  (and (eq? flag 'regular)
-       (= level 1)
-       (char=? (string-ref (basename filename) 0) #\_)
-       (begin
-	 ;;(format #t "Importing ~a\n" (basename filename))
-	 (with-input-from-file filename import-contact)))
-  #t)
-
-(define (import-contact)
-  (let loop ((contact '())
-	     (field-key #f)
-	     (field-value #f))
-    (let ((line (read-line)))
-      (if (eof-object? line)
-	  (begin
-	    ;; Send the contact to Emacs for further processing.
-	    (write (reverse! (acons field-key (reverse! field-value) contact)))
-	    (newline))		   
-	  (begin
-	    ;; Format does not allow empty lines.
-	    (assert (not (zero? (string-length line))))
-	    ;; Check for leading space.
-	    (if (char=? (string-ref line 0) #\space)
-		(begin
-		  ;; Start or continuation of a value.
-		  (assert field-key)
-		  (assert field-value)
-		  (loop contact
-			field-key
-			(cons (substring line 1) field-value)))
-		(begin
-		  ;; A new field, that may or may not have an
-		  ;; identifier.
-		  (let ((match-data (string-match "^([^ ]+) ?([^ ]*.*)$" line)))
-		    (assert match-data)
-		    (loop (if field-key
-			      (acons field-key (reverse! field-value) contact)
-			      contact)
-			  (string->symbol (match:substring match-data 1))
-			  (list (match:substring match-data 2)))))))))))
