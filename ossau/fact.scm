b40ee84865c19e9bfe7b862b052f916e5877abfb
Neil Jerram <neil@ossau.uklinux.net>
2010-11-17 21:20:29 +0000

    Add (ossau fact) module

---
+++ ossau/fact.scm
@@ -0,0 +1,15 @@
+(define-module (ossau fact)
+  #:export (fact1 fact2 facti))
+
+(define (fact1 n)
+  (if (= n 0)
+      1
+      (* n (fact1 (- n 1)))))
+
+(define (facti n a)
+  (if (= n 0)
+      a
+      (facti (- n 1) (* a n))))
+
+(define (fact2 n)
+  (facti n 1))
